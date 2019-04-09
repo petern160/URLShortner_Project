1db974285aa3f709e46e578e8f46ede74712a3c8
petern160 <peternguyen160@gmail.com>
2019-04-09 16:45:27 -0700

    added new :shortURL routethat links it to the website

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+var dogs = {
+    spot: {
+      name: 'Spot',
+      breed: 'Chihuahua'
+    },
+    jujube: {
+      name: 'Jujube',
+      breed: 'Chiweenie'
+    }
+  }
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,8 +33,13 @@ app.get('/urls', function(req, res){
 
 
 app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: req.params.shortURL };
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
+    var urlShort = req.params.name
+    var short = urlDatabase[urlShort]
     });
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
