646701a4affa532f4e72605977e55dad05901747
petern160 <peternguyen160@gmail.com>
2019-04-11 16:23:45 -0700

    added in bcrypt for password

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser')
 const bodyParser = require("body-parser");
-
+const bcrypt = require('bcrypt');
 
 // middleware
 app.use(bodyParser.urlencoded({extended: true}));
